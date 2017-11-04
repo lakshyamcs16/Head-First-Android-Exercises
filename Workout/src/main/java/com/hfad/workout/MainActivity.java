78f2480ceb46be9c2c541d1f25019b3cc908e417
lakshyamcs16 <32986427+lakshyamcs16@users.noreply.github.com>
2017-11-05 00:35:18 +0530

    Add files via upload

---
+++ Workout/src/main/java/com/hfad/workout/MainActivity.java
@@ -0,0 +1,26 @@
+package com.hfad.workout;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.FragmentTransaction;
+
+public class MainActivity extends Activity implements WorkoutListFragment.WorkoutListListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void itemClicked(long id) {
+        WorkoutDetailFragment details = new WorkoutDetailFragment();
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        details.setWorkoutId(id);
+        ft.replace(R.id.fragment_container, details);
+        ft.addToBackStack(null);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.commit();
+    }
+}
